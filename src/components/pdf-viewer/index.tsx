b36620b7cbe161066019da9c6f2f1e98d266c9c6
jraleman <jraleman@bendiburg.com>
2021-10-23 11:35:16 -0700

    Converts component to typescript, handles next/prev page

--- src/components/pdf-viewer/index.tsx
+++ src/components/pdf-viewer/index.tsx
@@ -1,23 +1,31 @@
 import { useEffect, useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import pdfjsWorker from "react-pdf/node_modules/pdfjs-dist/build/pdf.worker.entry";
+// @ts-ignore
+import pdfjsWorker from 'react-pdf/node_modules/pdfjs-dist/build/pdf.worker.entry';
 import './style.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
+interface IPdfViewer {
+  pdfPath: string;
+  timeout?: number;
+  opacity?: number;
+  disableRightClick?: boolean;
+}
 
 const PdfViewer = ({ 
   pdfPath = '',
   timeout = 0,
   opacity = 1,
   disableRightClick = false,
-}) => {
-  const [hide, setHide] = useState(false);
-  const [numPages, setNumPages] = useState(null);
+}: IPdfViewer) => {
+  const [hide, setHide] = useState<boolean>(false);
+  const [numPages, setNumPages] = useState<number | null>(null);
   const [pageNumber, setPageNumber] = useState(1);
 
-  function onDocumentLoadSuccess({ numPages }) {
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
     setNumPages(numPages);
-  }
+  };
   // useEffect(() => {
   //   const initPdf = async () => {
   //     const pdf = await loadPdf(pdfPath);
@@ -25,6 +33,18 @@ const PdfViewer = ({
   //   };
   //   initPdf();
   // }, [pdfPath]);
+
+  const handleNextPage = () => {
+    if (numPages && numPages > pageNumber) {
+      setPageNumber((n) => n += 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (pageNumber > 1) {
+      setPageNumber((n) => n -= 1);
+    }
+  };
 
   useEffect(() => {
     if (timeout) {
@@ -44,13 +64,15 @@ const PdfViewer = ({
   }
   return (
     <div style={{ opacity }}>
+      <p>Page {pageNumber} of {numPages}</p>
+      <button onClick={handlePrevPage}>Prev</button>
+      <button onClick={handleNextPage}>Next</button>
       <Document
         file={pdfPath}
         onLoadSuccess={onDocumentLoadSuccess}
       >
         <Page pageNumber={pageNumber} />
       </Document>
-      <p>Page {pageNumber} of {numPages}</p>
     </div>
   );
 }
