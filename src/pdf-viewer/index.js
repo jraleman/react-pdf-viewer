3e409da6a88bfe8d463e3686475126429585dc32
jraleman <jraleman@bendiburg.com>
2021-10-22 18:05:30 -0700

    Implements timeout functionality

--- src/pdf-viewer/index.js
+++ src/pdf-viewer/index.js
@@ -1,22 +1,41 @@
-import { useEffect } from 'preact/hooks';
-import "./style.css";
+import { useEffect, useState } from 'preact/hooks';
+import { loadPdf } from '../utils';
+import './style.css';
 
 const PdfViewer = ({ 
   pdfPath = '',
-  timeout = 0,
+  timeout = undefined,
   avoidCopy = false,
   opacity = 1,
 }) => {
+  const [hide, setHide] = useState(false);
+
   useEffect(() => {
     const initPdf = async () => {
       const pdf = await loadPdf(pdfPath);
       document.getElementById('pdf').src = pdf;
     };
     initPdf();
-  }, []);
+  }, [pdfPath]);
 
+  useEffect(() => {
+    if (timeout !== undefined) {
+      const seconds = timeout * 1000;
+      setTimeout(() => {
+        setHide(true);
+      }, seconds);
+    }
+  }, [timeout]);
+
+  useEffect(() => {
+    // TODO: implement avoidCopy functionality
+  }, [avoidCopy])
+
+  if (hide) {
+    return null;
+  }
   return (
-    <div>
+    <div style={{ opacity }}>
       <iframe id="pdf" className="pdf-frame" />
     </div>
   );
